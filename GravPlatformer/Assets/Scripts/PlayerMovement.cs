aff6ec8c7c3a31e5bac94fc694a58ac6e78b5af4
Fixtrit <cook1wc@cmich.edu>
2025-11-15 11:52:33 -0500

    Wyatt - updated player controllers. Added rocket controls and rocket exhaust, along with a gravity source script.

--- GravPlatformer/Assets/Scripts/PlayerMovement.cs
+++ GravPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 // 3: Drag the empty child game object into the playerMovement script in the player's inspector
 // 4: Set the ground layer to the desired ground layer in the player's inspector (this will determine what resets the ability to jump)
 
+// IMPORTANT NOTE: If you get an "InvalidOperations" error, then do the following vvv
+// In the Unity project, go to edit -> project settings -> player -> other settings -> active input handling and set it to both
+
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    private Vector2 groundCheckSize;
 
     private Rigidbody2D rb;
     private bool isGrounded;
@@ -19,12 +23,18 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        groundCheckSize = boxCollider.size;
+
+        groundCheckSize.x *= 1.1f;
+        groundCheckSize.y *= 2f;
     }
 
     void Update()
     {
         // Ground Check
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.3f, groundLayer);
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
 
         // Horizontal Movement
         float moveInput = Input.GetAxis("Horizontal");
