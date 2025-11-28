8977a12d4c2bf57fc55cb44bcdc09ae21b106227
BraydenKielb <btkielb09@gmail.com>
2025-11-28 13:32:35 -0500

    adding the finished LVL 1 design without working moving platforms

---
+++ GravPlatformer/Assets/Scripts/LRPlatform.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class LRPlatform : MonoBehaviour
+{
+	public float speed; 
+    public int startingPoint;
+	public Transform[] points;
+	private int i;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+		transform.position = points[startingPoint].position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Vector2.Distance(transform.position, points[i].position) < 0.02f){
+		 i++;
+		 if(i == points.Length){
+			 i = 0;
+		 }
+	 }
+	 transform.position = Vector2.MoveTowards(transform.position, points[i].position, 
+	 speed * Time.deltaTime);
+
+	}	 
+}
